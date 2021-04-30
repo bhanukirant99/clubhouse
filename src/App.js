d9ea49f7c2cfb3d799d516008148315731981399
Kushank Sriraj <kushanksriraj@gmail.com>
2021-04-30 21:54:18 +0530

    fix: removed limit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ const firestore = firebase.firestore();
 const auth = firebase.auth();
 
 const messagesRef = firestore.collection("messages");
-const query = messagesRef.orderBy("createdAt").limit(25);
+// const query = messagesRef.orderBy("createdAt").limit(25);
+const query = messagesRef.orderBy("createdAt");
 
 const ChatMessage = (props) => {
   const { text, uid, photoURL } = props.message;
