0482d83e4dbd9bf8610a010655062a55b3dacf82
Kushank Sriraj <kushanksriraj@gmail.com>
2021-04-30 09:44:34 +0530

    feat: added chat feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import firebase from "firebase";
+// import "firebase/firestore";
+// import "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { SignIn } from "./Components/SignIn";
+import { ChatRoom } from "./Components/ChatRoom";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBL5r5ym775zGClYTphPUA8zn4tvPZ--M4",
+  authDomain: "looptext-70c07.firebaseapp.com",
+  projectId: "looptext-70c07",
+  storageBucket: "looptext-70c07.appspot.com",
+  messagingSenderId: "55173207228",
+  appId: "1:55173207228:web:97203631080c63619eecb9",
+  measurementId: "G-M3YXEWGJ07",
+});
+
+const auth = firebase.auth();
+
+const SignOut = () => {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign out</button>
+  );
+};
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <SignOut />
       </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
